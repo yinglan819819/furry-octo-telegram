d895cd192d4346afaaef71fd068c342c3685c2d6
Yinglan Ou 🐳 <yinglao@amazon.com>
2021-05-03 22:27:37 -0700

    fix server.java line 17

--- src/main/java/com/mycompany/app/Server.java
+++ src/main/java/com/mycompany/app/Server.java
@@ -14,7 +14,7 @@ public class Server
     private void foo(String p) {
        String s = "aloha";
        System.out.println(String.format("This is a faulty message: %i", s));
-       System.out.format("No %s",1);
+       System.out.format("No %d",1);
        if ("1" == "2") {
             //dosomething
        }
